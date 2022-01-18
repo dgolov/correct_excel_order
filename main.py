0b86bd3e777b5937e2ff9a9e605fc7726fa9b965
dgolov <dgolov@icloud.com>
2022-01-19 00:25:29 +0300

    Refactoring

--- main.py
+++ main.py
@@ -2,33 +2,40 @@ from openpyxl import load_workbook
 import json
 
 
-def open_excel():
-    print('Загрузка документа...')
-    wb = load_workbook('./test2.xlsx')
+def open_excel(path: str, sheet_name: str) -> (tuple, None):
+    print('Loading document...')
+    wb = load_workbook(path)
+    print(type(wb))
     try:
-        sheet = wb.get_sheet_by_name('Выгрузка')
+        sheet = wb.get_sheet_by_name(sheet_name)
+        print(type(sheet))
         return wb, sheet
     except KeyError:
         return None
 
 
-def correcting_file(wb, sheet):
-    print('Парсинг документа...')
+def correcting_file(wb, sheet, row_key: int, row_value: int) -> None:
+    print('Parsing document...')
     for row in range(1, sheet.max_row):
-        result = data.get(sheet.cell(row=row, column=8).value)
+        result = data.get(sheet.cell(row=row, column=row_key).value)
         if result:
-            sheet.cell(row=row, column=6).value = result
+            sheet.cell(row=row, column=row_value).value = result
 
-    wb.save('./test3.xlsx')
-    print('Успешно!')
+    wb.save('./correct_order.xlsx')
+    print('Success!')
+    return
 
 
-with open('data.json', 'r') as file:
+with open('data2.json', 'r') as file:
+    print('Open json file')
     data = json.load(file)
 
 
 try:
-    work_book, sheet_name = open_excel()
-    correcting_file(work_book, sheet_name)
+    order = open_excel(
+        path='./mba/order.xlsx',
+        sheet_name="Выгрузка"
+    )
+    correcting_file(*order, row_key=8, row_value=6)
 except TypeError:
-    print('Неверное имя "Выгрузка"')
+    print('Incorrect sheet name')
